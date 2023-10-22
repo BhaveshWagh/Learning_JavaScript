4bb29386803fdc6f7707c1f2766d1c38932d4688
BhaveshWagh <bhaveshbm2711@gmail.com>
2023-10-22 20:27:19 +0530

    interview questions

---
+++ Ask-In-Interview/array.js
@@ -0,0 +1,14 @@
+// Display the original properties of array
+// Prototype
+
+const array = [1,2,3,4,5]
+
+for (const key in array) {
+    if (Object.hasOwnProperty.call(array, key)) {
+        const element = array[key];
+        console.log(element)
+    }
+}
+// array.forEach(element => {
+//     console.log(element)
+// });
