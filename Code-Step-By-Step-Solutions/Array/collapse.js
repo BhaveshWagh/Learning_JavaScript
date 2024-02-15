87738f249022f971a63088a54b45b1a57c2a2a95
BhaveshWagh <bhaveshbm2711@gmail.com>
2024-02-15 16:27:53 +0530

    expressions : avgOf3

--- Code-Step-By-Step-Solutions/Array/collapse.js
+++ Code-Step-By-Step-Solutions/Array/collapse.js
@@ -1,23 +1,34 @@
-
-function collapse(arr){
-    let arr2 = []
-    let item = 0
-    for (let i = 0; i < arr.length; i+=2){
-        if(arr[i] + arr[i + 1] === 0){
-            arr2.push(0)
-        }
-        else{
-
-            item = arr[i] + arr[i + 1] || arr[i]
-            arr2.push(item)
-        }
-        // console.log(item)
-        
+function collapse(arr) {
+  let arr2 = [];
+  let item = 0;
+  for (let i = 0; i < arr.length; i += 2) {
+    if (arr[i] + arr[i + 1] === 0) {
+      arr2.push(0);
+    } else {
+      item = arr[i] + arr[i + 1] || arr[i];
+      arr2.push(item);
     }
-    return arr2
+    // console.log(item)
+  }
+  return arr2;
 }
 
+const arr = [100, -100];
+console.log(collapse(arr));
 
 
-const arr = [100,-100]
-console.log(collapse(arr))
+// function collapse(arr) {
+    // const result = [];
+
+    // Iterate through pairs of integers in the array
+    // for (let i = 0; i < arr.length - 1; i += 2) {
+        // result.push(arr[i] + arr[i + 1]);
+    // }
+
+    // If the array has an odd length, append the last element as is
+    // if (arr.length % 2 !== 0) {
+        // result.push(arr[arr.length - 1]);
+    // }
+
+    // return result;
+// }
---
+++ Code-Step-By-Step-Solutions/expression/averageOf3.js
@@ -0,0 +1,6 @@
+function averageOf3(num1,num2,num3){
+    let sum = num1 + num2 + num3
+    let avg = sum / 3
+    return avg
+}
+console.log(averageOf3(6,14,4))
