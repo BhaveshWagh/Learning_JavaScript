3ca897c0c1a9acdc1e32548088b33447a2a0960d
BhaveshWagh <bhaveshbm2711@gmail.com>
2024-03-03 11:47:16 +0530

    reverse String

---
+++ CoderByteProblems/ReverseString.js
@@ -0,0 +1,10 @@
+function FirstReverse(str) { 
+
+    // code goes here  
+     var reverseStr = str.split('').reverse().join('')
+     
+     return reverseStr;
+  }
+     
+  // keep this function call here 
+  console.log(FirstReverse("Hello"));
