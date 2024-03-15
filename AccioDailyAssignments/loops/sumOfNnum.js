f7c85be49204f1e72fb444467d335fd3e608f55d
BhaveshWagh <bhaveshbm2711@gmail.com>
2024-03-15 22:33:50 +0530

    Day04 Problems solved

--- AccioDailyAssignments/loops/sumOfNnum.js
+++ AccioDailyAssignments/loops/sumOfNnum.js
@@ -7,8 +7,12 @@ const rl = readline.createInterface({
 
 rl.on('line', (N) => {
   // write your code here
-    let num = Number(N)
-    let sumOfNNaturals = num*(num+1)/2
+    // let num = Number(N)
+    // let sumOfNNaturals = num*(num+1)/2
+    let sum = 0;
+    for(let i = 1; i <= N; i++ ){
+        sum += i
+    }
     console.log(sumOfNNaturals);
   rl.close();
 });
