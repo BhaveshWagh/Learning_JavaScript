55fb3dc59dbf958627601405cdf64df3f33e719b
BhaveshWagh <bhaveshbm2711@gmail.com>
2024-04-28 20:38:02 +0530

    maze path

---
+++ AccioDailyAssignments/Module_02_JS_Interm_DSA_April_24/Recursion_Day_05_Revision/aMazePaths.js
@@ -0,0 +1,30 @@
+// aMaze paths
+// https://course.acciojob.com/idle?question=bb073cbd-0873-4607-824f-35edc0041c02
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+function count(n, m, i = 1, j = 1, ans = "") {
+  // console.log(i,j)
+  if (i == n && j === m) {
+    console.log(ans);
+    return 1;
+  }
+  if (i > n || j > m) {
+    return 0;
+  }
+
+  return count(n, m, i, j + 1, ans + "h") + count(n, m, i + 1, j, ans + "v");
+}
+
+rl.question("", (n) => {
+  rl.question("", (m) => {
+    // console.log(Number(n),Number(m))
+    // const [n, m] = input.split(' ').map(Number);
+    count(Number(n), Number(m));
+    rl.close();
+  });
+  rl.close();
+});
