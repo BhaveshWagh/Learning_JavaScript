3d648816ba37fc04d1245f5d765cd2d387476480
BhaveshWagh <bhaveshbm2711@gmail.com>
2024-06-13 21:03:54 +0530

    substring brutforce approach

--- AccioDailyAssignments/Gym/String/Substring.js
+++ AccioDailyAssignments/Gym/String/Substring.js
@@ -4,6 +4,29 @@ function doesItContain(line1, ch) {
     // write code here
     // return the output , do not use console.log here
       return line1.includes(ch);
+
+
+    //   another aprroach
+//     let n = line1.length;
+//   let m = ch.length;
+  
+//   if (m > n) {
+//     return false;
+//   }
+  
+//   for (let i = 0; i <= n - m; i++) {
+//     let j;
+//     for (j = 0; j < m; j++) {
+//       if (line1[i + j] !== ch[j]) {
+//         break;
+//       }
+//     }
+//     if (j === m) {
+//       return true;
+//     }
+//   }
+  
+//   return false;
   }
   
   const rl = require("readline");
