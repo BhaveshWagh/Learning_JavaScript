fa4caa1e1ce1ce62bca9cab5a26393c64dee86e7
BhaveshWagh <bhaveshbm2711@gmail.com>
2024-08-07 09:36:36 +0530

    gym : bubble sort Ascending && Descending

---
+++ Ask-In-Interview/SortingAlogrithms/bubbleSortDesc.js
@@ -0,0 +1,16 @@
+function bubbleSortDesc(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length; j++) {
+      if (arr[j] < arr[j + 1]) {
+        // swap it
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+  return arr;
+}
+
+const inpArr = [1, 4, 2, 3, 5, -1, -2];
+console.log(bubbleSortDesc(inpArr));
