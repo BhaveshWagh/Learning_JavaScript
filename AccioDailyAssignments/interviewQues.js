21534303d8aadb53ef1c3b3cb8b11054d25dbc27
BhaveshWagh <bhaveshbm2711@gmail.com>
2024-11-21 19:07:14 +0530

    HOF: Higher Order Functions

--- AccioDailyAssignments/interviewQues.js
+++ AccioDailyAssignments/interviewQues.js
@@ -222,7 +222,7 @@
 
 // console.log(y);
 
-// In the above example, the assign operator knows that the value assigned to y is a primitive type (number type in this case), so when the second line code executes, where the value of y is assigned to z, the assign operator takes the value of y (10) and allocates a new space in the memory and returns the address. Therefore, variable z is not pointing to the location of variable y, instead, it is pointing to a new location in the memory.
+// In the above example, the assign operator knows that the value assigned to y is a primitive type (number type in this case), so when the second line code executes, wher the value of y is assigned to z, the assign operator myparam the value of y (10) and allocates a new space in the memory and returns the address. Therefore, variable z is not pointing to the location of variable y, instefad, it is pointing to a new location in the memory.
 
 // From the above example, we can see that primitive data types when passed to another variable, are passed by value. Instead of just assigning the same address to another variable, the value is passed and new space of memory is created.
 
@@ -271,8 +271,107 @@
 
 // Therefore, non-primitive data types are always passed by reference
 
-
 // var x = 10;
 // // z = 100;
 // // console.log(x,z)
 // console.log(this)
+
+// 9. Explain Higher Order Functions in Javascript
+// Ans: Functions that operate on other functions, either by taking them as arguments or by returning them, are called higher order functions.
+// * A higher order function is a function that takes one or more functions as arguments, or returns a function as its result.A higher order function is a function that takes one or more functions as arguments, or returns a function as its result.
+
+// There are several different types of higher order functions like map and reduce. We will discuss these later in this tutorial. But before that let's first dive deep into what higher order functions are.
+
+// Example
+// Callback function, passed as a parameter in the higher order function
+function callbackFunction() {
+  console.log("I am callback function");
+}
+
+// higher order function
+function higherOrderFunction(func) {
+  console.log("I am higher order function");
+  func();
+}
+
+higherOrderFunction(callbackFunction); // I am higher order function
+// I am callback function
+
+// In the above code higherOrderFunction() is an HOF because we are passing a callback function as a parameter to it.
+
+// The above example is quite simple isn't it? So let's expand it further and see how you can use HOFs to write more concise and modular code.
+
+// How Higher Order Functions Work
+// So, suppose I want you to write a function that calculates the area and diameter of a circle. As a beginner, the first solution that comes to our mind is to write each separate function to calculate area or diameter.
+
+const radius = [1, 2, 3];
+
+// function to calculate area of circle;
+const areaOfCircle = function (radius) {
+  const result = [];
+  for (let i = 0; i < radius.length; i++) {
+    result.push(Math.PI * radius[i] * radius[i]);
+  }
+  return result;
+};
+
+// function to calculate diameter of the circle;
+const diameterOfCircle = function (radius) {
+  const result = [];
+  for (let i = 0; i < radius.length; i++) {
+    result.push(2 * radius[i]);
+  }
+  return result;
+};
+
+console.log(areaOfCircle(radius)); // [ 3.141592653589793, 12.566370614359172, 28.274333882308138 ]
+console.log(diameterOfCircle(radius)); // [ 2, 4, 6 ]
+
+// But have you noticed the problem with the above code? Aren't we writing almost the same function again and again with slightly different logic? Also, the functions we have written aren't reusable, are they?
+
+// So, let's see how we can write the same code using HOFs:
+
+// logic to clculate area
+const area = function (radius) {
+  return Math.PI * radius * radius;
+};
+
+// logic to calculate diameter
+const diameter = function (radius) {
+  return 2 * radius;
+};
+
+// a reusable function to calculate area, diameter, etc;
+const calculate = function (radius, logic) {
+  const output = [];
+  for (let i = 0; i < radius.length; i++) {
+    output.push(logic(radius[i]));
+  }
+  return output;
+};
+
+console.log(calculate(radius, area)); // [ 3.141592653589793, 12.566370614359172, 28.274333882308138 ]
+console.log(calculate(radius, diameter)); // [ 2, 4, 6 ]
+
+// Now, as you can see in the above code, we have only written a single function calculate() to calculate the area and diameter of the circle. We only need to write the logic and pass it to calculate() and the function will do the job.
+
+// The code that we have written using HOFs is concise and modular. Each function is doing its own job and we are not repeating anything here.
+
+// Suppose in the future if we want to write a program to calculate the circumference of the circle. We can simply write the logic to calculate the circumference and pass it to the calculate() function.
+
+const circumeference = function (radius) {
+  return 2 * Math.PI * radius;
+};
+console.log(calculate(radius, circumeference));
+
+// How to Use Higher Order Functions
+// You can use higher order functions in a variety of ways.
+
+// When working with arrays, you can use the map(), reduce(), filter(), and sort() functions to manipulate and transform data in an array.
+
+// When working with objects, you can use the Object.entries() function to create a new array from an object.
+
+// When working with functions, you can use the compose() function to create complex functions from simpler ones.
+
+// If you want to know how to Use Some Important Higher Order Function like -> map(), reduce(), & filter()
+// ref: This Readme file : https://github.com/BhaveshWagh/Learning_JavaScript/blob/main/AccioDailyAssignments/Interview-Questions.md
