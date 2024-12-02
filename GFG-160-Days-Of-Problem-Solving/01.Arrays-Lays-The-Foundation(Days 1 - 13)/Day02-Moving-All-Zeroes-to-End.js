53a99725675a39bf919e919af310fbc1dc7739c8
BhaveshWagh <bhaveshbm2711@gmail.com>
2024-12-02 14:45:17 +0530

    Expected Approach

--- GFG-160-Days-Of-Problem-Solving/01.Arrays-Lays-The-Foundation(Days 1 - 13)/Day02-Moving-All-Zeroes-to-End.js
+++ GFG-160-Days-Of-Problem-Solving/01.Arrays-Lays-The-Foundation(Days 1 - 13)/Day02-Moving-All-Zeroes-to-End.js
@@ -30,34 +30,56 @@ Explanation: No change in array as there are all 0s.
 // Time complexity: O(n), as we are traversing the array three times.
 // Auxiliary Space : O(n), as we are using a temp[] array to move all the zeros.
 
-function pushZerosToEnd(arr) {
-  //code
-  const n = arr.length;
-  const temp = new Array(n);
+// function pushZerosToEnd(arr) {
+//   //code
+//   const n = arr.length;
+//   const temp = new Array(n);
 
-  let j = 0;
+//   let j = 0;
 
-  for (let i = 0; i < n; i++) {
-    if (arr[i] !== 0) {
-      temp[j++] = arr[i];
-      // console.log(temp)
-    }
-  }
-  // console.log(j)
-  while (j < n) {
-    temp[j++] = 0;
-    // console.log(temp)
-  }
-  // copy from temp[] to arr[]
-  for (let i = 0; i < n; i++) {
-    arr[i] = temp[i];
-  }
-  console.log(arr);
-}
+//   for (let i = 0; i < n; i++) {
+//     if (arr[i] !== 0) {
+//       temp[j++] = arr[i];
+//       // console.log(temp)
+//     }
+//   }
+//   // console.log(j)
+//   while (j < n) {
+//     temp[j++] = 0;
+//     // console.log(temp)
+//   }
+//   // copy from temp[] to arr[]
+//   for (let i = 0; i < n; i++) {
+//     arr[i] = temp[i];
+//   }
+//   console.log(arr);
+// }
 
 // arr = [1, 2, 0, 4, 3, 0, 5, 0];
 // arr = [10, 20, 30]
 // arr = [0,0]
 
+// ! EXPECTED APPROACH :   One Traversal – O(n) Time and O(1) Space
+
+// Time Complexity: O(n), as we are traversing the array only once.
+// Auxiliary Space: O(1)
+
+/**
+ * *The idea is similar to the previous approach where we took a pointer, say count to track where the next non-zero element should be placed. However, on encountering a non-zero element, instead of directly placing the non-zero element at arr[count], we will swap the non-zero element with arr[count]. This will ensure that if there is any zero present at arr[count], it is pushed towards the end of array and is not overwritten.
+ */
+
+function pushZerosToEnd(arr) {
+  // Pointer to track the position for next non-zero element
+  let count = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] !== 0) {
+      [arr[i], arr[count]] = [arr[count], arr[i]];
+      count++;
+    }
+  }
+}
+
 arr = [3, 5, 0, 0, 4];
 pushZerosToEnd(arr);
+console.log(arr.join(" "));
