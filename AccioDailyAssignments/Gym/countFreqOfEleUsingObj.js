bdcb3568a1f2cc4cb168d8b9fa78e356a2616c32
BhaveshWagh <bhaveshbm2711@gmail.com>
2024-12-17 22:34:22 +0530

    problem : Max Frequency of Character

--- AccioDailyAssignments/Gym/countFreqOfEleUsingObj.js
+++ AccioDailyAssignments/Gym/countFreqOfEleUsingObj.js
@@ -1,11 +1,27 @@
-const obj = {};
-const arr = [1, 1, 2, 2, 1, 3, 4, 5];
+function maxOccurence(arr) {
+  // const obj = {};
+  const obj = new Object();
+  let max_freq = 0;
+  let max_freq_char = "";
 
-for (let item of arr) {
-  if (obj[item]) {
-    obj[item] += 1;
-  } else {
-    obj[item] = 1;
+  for (let item of arr) {
+    if (obj[item]) {
+      obj[item] += 1;
+    } else {
+      obj[item] = 1;
+    }
   }
+
+  for (let key in obj) {
+    if (obj[key] >= max_freq) {
+      max_freq = obj[key];
+      max_freq_char = key;
+    }
+  }
+  console.log(`character: ${max_freq_char}, max occurence : ${max_freq} `);
+  console.log(obj);
 }
-console.log(obj);
+const arr = [1, 1, 2, 2, 1, 3, 4, 5];
+const str = "Bhavesh Wagh";
+maxOccurence(str);
+maxOccurence(arr);
